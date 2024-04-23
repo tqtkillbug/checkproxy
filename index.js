5e2b5d43b37c0555cb862aa67b4d8c523bc43324
TienDapChai <tqtteams1st@gmail.com>
2024-04-24 00:18:21 +0700

    Get ip from host server

--- index.js
+++ index.js
@@ -57,6 +57,19 @@ app.post('/api/info', async (req, res) => {
     res.status(200).send(result);
 })
 
+app.post('/api/info/ip', async (req, res) => {
+    var proxy = req.body.proxy;
+    var split = proxy.split(":");
+    if (split.length !== 4) return "{s}";
+    let host = split[0];
+    let port = split[1];
+    var result = host;
+    if (host.indexOf(".com") > 0) {
+        result = await getIpByProxy(`${host}:${port}`);
+    }
+    res.status(200).send(result);
+})
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
@@ -82,7 +95,6 @@ const getInfo = async (ip) => {
     };
 
     try {
-        // Sử dụng promise để gửi yêu cầu HTTP đồng bộ
         const body = await new Promise((resolve, reject) => {
             REQUEST(options, function (error, response, body) {
                 if (error) {
@@ -98,6 +110,38 @@ const getInfo = async (ip) => {
         return "{}";
     }
 };
+
+const getIpByProxy = async (host) => {
+    const proxyUrl = 'http://' + host;
+    const proxyUsername = '';
+    const proxyPassword = '';
+    const options = {
+        url: 'https://api.ipify.org',
+        proxy: proxyUrl,
+        auth: {
+            user: proxyUsername,
+            pass: proxyPassword
+        }
+    };
+
+    try {
+        const body = await new Promise((resolve, reject) => {
+            REQUEST(options, function (error, response, body) {
+                if (error) {
+                    reject(error);
+                } else {
+                    resolve(body);
+                }
+            });
+        });
+        return body;
+    } catch (error) {
+        return "{}";
+    }
+}
+
+
+
 
 
 
